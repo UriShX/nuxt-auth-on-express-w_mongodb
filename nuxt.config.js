d3f6609384d6f6fc65db21826a514f44d0983be8
UriShX <usdogi@gmail.com>
2020-05-26 18:00:04 +0300

    test changing axios base url to null

--- nuxt.config.js
+++ nuxt.config.js
@@ -56,7 +56,9 @@ module.exports = {
    */
   axios: {
     baseURL:
-      process.env.NODE_ENV !== 'production' ? 'http://localhost:3000/api' : '',
+      process.env.NODE_ENV !== 'production'
+        ? 'http://localhost:3000/api'
+        : null,
     prefix: process.env.NODE_ENV === 'production' ? '/api' : ''
   },
   /*
