71c33d20166d37647902ba305b4a848526917c18
UriShX <usdogi@gmail.com>
2020-05-26 21:40:41 +0300

    set baseURL in server/index.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -54,7 +54,9 @@ module.exports = {
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
    */
-  axios: {},
+  axios: {
+    baseURL: 'http://localhost:3000/api'
+  },
   /*
    ** vuetify module configuration
    ** https://github.com/nuxt-community/vuetify-module
