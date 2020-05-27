56f46b3dfecb429f4d64606809eaba1a1a8907b9
UriShX <usdogi@gmail.com>
2020-05-27 12:08:27 +0300

    remove axios.baseURL from nuxt.config

--- nuxt.config.js
+++ nuxt.config.js
@@ -55,7 +55,7 @@ module.exports = {
    ** See https://axios.nuxtjs.org/options
    */
   axios: {
-    baseURL: 'http://localhost:3000/api'
+    // baseURL: 'http://localhost:3000/api'
   },
   /*
    ** vuetify module configuration
